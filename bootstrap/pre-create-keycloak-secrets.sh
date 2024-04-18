bd35ca8ab07d9ade2d5e32479546397294d7fd35
Momoah <momoah@github.com>
2024-04-19 08:16:53 +1000

    changed the route again

--- bootstrap/pre-create-keycloak-secrets.sh
+++ bootstrap/pre-create-keycloak-secrets.sh
@@ -7,8 +7,8 @@ subjectAltName = @alt_names
  
 [ alt_names ]
 DNS.1 = keycloak.local.momolab.io
-DNS.2 = keycloak-rhbk.apps.sno1.local.momolab.io
-DNS.3 = keycloak-rhbk.apps.sno2.local.momolab.io
+DNS.2 = keycloak-admin-route-rhbk.apps.sno1.local.momolab.io
+DNS.3 = keycloak-admin-route-rhbk.apps.sno2.local.momolab.io
 EOF
  
 openssl req -x509 \
